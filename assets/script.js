f1e7a0014ed67133436bf631a83abddfb4e705e3
Meng Po <pengitoast@gmail.com>
2022-02-16 19:24:57 -0700

    first try

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Adrian Smith" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
@@ -12,16 +12,26 @@ let sugar = 0 // Sugar Sprinkle
 const credit = document.querySelector('#credit')
 // selecting the element with an id of add-gb
 const gbPlusBtn = document.querySelector('#add-gb')
-
+const gbMinusBtn = document.querySelector("#minus-gb")
+const gbAmount = document.querySelector('#qty-gb')
+const ccPlusBtn = document.querySelector("#add-cc")
+const ccMinusBtn = document.querySelector('#minus-cc')
+const ccAmount = document.querySelector('#qty-cc')
+const sugarPlusBtn = document.querySelector('#add-sugar')
+const sugarMinBtn = document.querySelector('#minus-sugar')
+const sugarAmount = document.querySelector('#qty-sugar')
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
+gbMinusBtn.addEventListener("onclick", function(){
+    if (gb > 0); (gb - 1)
+}
+gbPlusBtn.addEventListener("onclick", function(){
+    (gbAmount = gb + 1)
+}
 // HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
 
 // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-})
 
 // TODO: Hook up event listeners for the rest of the buttons
